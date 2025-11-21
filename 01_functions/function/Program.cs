10a86f7393549a6beea5c14c963300a895aa2f42
FxnnBT <40134@ma-web.nl>
2025-11-21 14:30:50 +0100

    function

---
+++ 01_functions/function/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program(); // de 2e program is de variabele
+            program.Run();
+        }
+
+         internal void Run()
+        {
+            Console.WriteLine("program gestart");
+            return; // stopt de functie
+        }
+
+    }
+}
