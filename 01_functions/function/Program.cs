2c1823f7f996fba76b8488f1e97e73e6734fe059
FxnnBT <40134@ma-web.nl>
2025-11-21 14:43:14 +0100

    readlines add

--- 01_functions/function/Program.cs
+++ 01_functions/function/Program.cs
@@ -11,7 +11,48 @@
          internal void Run()
         {
             Console.WriteLine("program gestart");
+            vraag1();
+            vraag2();
+            vraag3();
+            vraag4();
+            vraag5();
+            Console.WriteLine("Dat was t de ballen");
             return; // stopt de functie
+        }
+        internal void vraag1()
+        {
+            Console.WriteLine("hoeveel mannen zijn door bonnie blue heen gegaan");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+            return;
+        }
+        internal void vraag2()
+        {
+            Console.WriteLine("Hoeveel views heeft de meest bekeken video op youtube");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+            return;
+        }
+        internal void vraag3()
+        {
+            Console.WriteLine("Wat is de hoofdstad van nederland");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+            return;
+        }
+        internal void vraag4()
+        {
+            Console.WriteLine("Wat is de beste programmeertaal");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+            return;
+        }
+        internal void vraag5()
+        {
+            Console.WriteLine("Wat is beter powershell of cmd");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+            return;
         }
 
     }
