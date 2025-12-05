a766582200e23b313a212a7684529df7f27af113
FxnnBT <40134@ma-web.nl>
2025-12-05 14:19:14 +0100

    00 constructor

---
+++ 03_constructors/Constructors/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Constructors;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+    private void Run()
+    {
+        QuizVraag quizVraag = new QuizVraag("hier komt de vraag", "hier komt het antwoord");
+    }
+}
