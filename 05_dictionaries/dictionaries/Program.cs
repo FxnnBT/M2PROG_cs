bc7b61df906dab76103ea47fd052f4b24821f298
FxnnBT <40134@ma-web.nl>
2026-01-09 14:57:59 +0100

    rooms

--- 05_dictionaries/dictionaries/Program.cs
+++ 05_dictionaries/dictionaries/Program.cs
@@ -2,6 +2,8 @@
 
 class Program
 {
+    private Dictionary<string, Room> rooms = new Dictionary<string, Room>();
+
     static void Main(string[] args)
     {
         Program program = new Program();
@@ -11,11 +13,19 @@ class Program
     void Run()
     {
         dictionaries();
+        Rooms();
+        ShowRoom(0, 0);
+        ShowRoom(1, 0);
+        ShowRoom(1, 1);
+        ShowRoom(0, 1);
+        ShowRoom(2, 2);
+        
     }
 
-    internal void dictionaries(){
+    internal void dictionaries()
+    {
         Dictionary<int, string> personeelopnummer = new Dictionary<int, string>();
-        
+
         personeelopnummer.Add(69, "John pork");
         personeelopnummer.Add(67, "Jane Doe");
 
@@ -37,4 +47,62 @@ class Program
             Console.WriteLine(value);
         }
     }
+
+
+    internal void Rooms()
+    {
+        Room start = new Room(0, 0, [
+        "#####",
+        "#   #"
+        ,"#   #"
+        ,"#   #"
+        ,"#####",]);
+
+        Room room1 = new Room(1, 0, [
+        "#####",
+        "#   #"
+        ,"#   #"
+        ,"#   #"
+        ,"#####",]);
+
+        Room room2 = new Room(1, 1, [
+        "#####",
+        "#   #"
+        ,"#   #"
+        ,"#   #"
+        ,"#####",]);
+        Room room3 = new Room(0, 1, [
+        "#####",
+        "#   #"
+        ,"#   #"
+        ,"#   #"
+        ,"#####",]);
+
+        rooms.Add(start.GetRoomLocationKey(), start);
+        rooms.Add(room1.GetRoomLocationKey(), room1);
+        rooms.Add(room2.GetRoomLocationKey(), room2);
+        rooms.Add(room3.GetRoomLocationKey(), room3);
+    }
+
+    private void ShowRoom(int x, int y)
+    {
+        bool hasRoom = rooms.ContainsKey($"{x},{y}");
+        if (hasRoom)
+        {
+            Room room = rooms[$"{x},{y}"];
+            Console.WriteLine($"You are in room {x},{y}");
+            foreach (string r in room.level)
+            {
+                Console.WriteLine(r);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error 404 Room not found");
+        }
+    }
+
+
+
+
 }
