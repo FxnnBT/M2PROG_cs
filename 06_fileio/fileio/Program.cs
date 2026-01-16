0399307f8027053f09cce5cc3a552ba9262f9ef5
FxnnBT <40134@ma-web.nl>
2026-01-16 14:18:16 +0100

    NOS

--- 06_fileio/fileio/Program.cs
+++ 06_fileio/fileio/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program program = new Program();
+        program.Run();
     }
+    void Run()
+    {
+        ReadFile();
+    }
+
+    private void ReadFile()
+    {
+        /*Console.WriteLine(Environment.CurrentDirectory);
+        string[] lines = File.ReadAllLines("quiz.txt");
+        for (int i = 0; i < lines.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}: {lines[i]}");
+        } */
+
+        Console.WriteLine(Environment.CurrentDirectory);
+        string[] lines = File.ReadAllLines("nos.txt");
+        for (int i = 0; i < lines.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}: {lines[i]}");
+        }
+    }
+
 }
