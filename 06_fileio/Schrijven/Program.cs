7f933c2920eb0fce396a18cc8f837357ae741c7c
FxnnBT <40134@ma-web.nl>
2026-01-23 14:09:15 +0100

    favo game

---
+++ 06_fileio/Schrijven/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Schrijven;
+using System;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Run();
+    }
+    internal void Run()
+    {
+        string[] lines = { "Regel 1", "Regel 2", "Regel 3" };
+        File.WriteAllLines("mijnregels.txt", lines);
+
+        string text = File.ReadAllText("Favogame.json");
+        FavoriteGame[] favogames = JsonSerializer.Deserialize<FavoriteGame[]>(text);
+        foreach (FavoriteGame game in favogames)
+        {
+            Console.WriteLine(game.Name);
+            Console.WriteLine(game.Platform);
+        }
+        //File.WriteAllText("mijntekst.txt", "Dit is een test");
+    }
+}
